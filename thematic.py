f67afc7f29e5faceefab1b9a1e07b8efbde4f130
Nathaniel V. KELSO <nvkelso@gmail.com>
2012-05-05 03:11:36 -0700

    better organization, works as master controller now

--- thematic.py
+++ thematic.py
@@ -12,37 +12,60 @@
 # Cascadenik CSS style map rendering in Mapnik.
 
 import os, sys
-import math, stat
 from optparse import OptionParser
+import csv
+import math, stat
+
 try:
   from osgeo import gdal, ogr  
 except ImportError:
   import gdal
   import ogr
-#from import Thematic.ogrinfo import Ogrinfo
-from Thematic.quantile import Quantile
+  
 from Thematic.classify import Classify
+#from import Thematic.ogrinfo import OgrInfo
 
 
 optparser = OptionParser(usage="""%prog [options]
 
-OGR based Python tools for querrying a data source (SHP, etc) and drawing 
-# thematic cartographic representations.""")
+THEMATIC CARTO TOOLS
 
-optparser.add_option('-s', '--data_file', dest='infilename',
-                  help='Give me your huddled masses of geodata.')
-optparser.add_option('-o', '--out_file', dest='outfilename', default='stylesheet',
-                  help='Style name for resulting MSS and MML files.')                  
-optparser.add_option('-i', '--indicator', dest='fieldname', 
-                  help='Data is in which column.')
-optparser.add_option('-l', '--legend-type', dest='legend_type', 
-                  help='Valid types are: single-symbol, unique-value, continuous-color, and graduated-symbol.')
+Is a CSV, SHP, and OGR based Python tools for querrying a data source and drawing 
+thematic cartographic representations.""")
+
+optparser.add_option('-s', '--in_file','--data_file', dest='infilename',
+                      help='Give me your huddled masses of geodata!')
+
+optparser.add_option('-o', '--out_files', dest='outfiles', action='append', nargs=3, default=('stylesheet.css', 'style.mml', 'legend.html'),
+                      help='Style name for resulting MSS, MML, and HTML files.')
+
+optparser.add_option('-i', '--indicator', '--field-name', dest='field_name', 
+                      help='Data is in which column.')
+
+optparser.add_option('-m', '--measurement', dest='measurement', default='quantitative',
+                      help='quantitative or qualitative.')
+
+optparser.add_option('--name-field', dest='name_field', default='name',
+                      help='Optional name of column for labels to name themselves. Default value is "name".')
+
+optparser.add_option('--filter-field', dest='filter_field', action='append', nargs=2,
+                      help='Field to use for limiting selection by theme and the value to limit by. Default is no filter. Useful if more than one enumeration unit type is present in master data file (eg: mixed ZIPs and Counties).')
+
+optparser.add_option('-l', '--legend-type', dest='legend_type', default='tell-me',
+                      help='Valid types are: tell-me (tk), single-symbol, unique-value, bins, continuous-color, and graduated-symbol.')
+
 optparser.add_option('-c', '--classification-type', dest='class_type', 
-                  help='Valid types are: quantiles, tk tk tk.')
+                      help='Valid types are: quantiles, tk tk tk.')
+
 optparser.add_option('-n', '--number-breaks', dest='num_breaks', default=5, type='int',
-                  help='Number of data breaks. single-symbol=1 by default.')
+                      help='Number of data breaks. single-symbol=1 by default.')
+
 optparser.add_option('-r', '--colors', dest='colors', default='YlGnBu',
-                  help='From ColorBrewer.org')
+                      help='Named color series from ColorBrewer.org or other--color-space (done), or comma-separated-values for specific #aabbcc; hex values (tk)')
+
+optparser.add_option('--color-space', dest='color-space', default='ColorBrewer',
+                      help='ColorBrewer, Hex (tk), Kuler (tk)')
+
 
 if __name__ == "__main__":
 
@@ -54,189 +77,233 @@ if __name__ == "__main__":
     
     # Input geodata
     in_dir = os.path.dirname( os.path.abspath( options.infilename ) )
-    in_file = os.path.basename( os.path.abspath( options.infilename ) )
+    in_file, in_file_extension = os.path.splitext( os.path.abspath( options.infilename ) )
+    in_file_name_part, in_file_ext_part = os.path.basename( os.path.abspath( options.infilename ) ).split('.')
     in_file_fullpath = os.path.abspath( options.infilename )
     
-    # Output MSS and MML files
-    out_dir = os.path.dirname( os.path.abspath( options.outfilename ) )
-    out_mss = options.outfilename + '.mss'
-    out_mml = options.outfilename + '.mml'
+    print 'options.outfiles: ', options.outfiles
+    
+    # Output MSS, MML, and HTML files
+    out_dir = os.path.dirname( os.path.abspath( options.outfiles[0] ) )
+    out_mss = options.outfiles[0]
+    out_mml = options.outfiles[1]
+    out_html = options.outfiles[2]
     
     # Store the options
-    fieldname = options.fieldname
+    indicator_field = options.field_name
+    if options.measurement == 'quantitative': 
+        quantitative = True
+    else: 
+        quantitative = False
+    feature_name_field = options.name_field
     legend_type = options.legend_type
+    classification_type = options.class_type
+    filter_field = options.filter_field
+        
+        
+    # init the cleaned data holder
+    data_clean = []
+
+    print 'in_file_extension: ', in_file_extension
     
+    # gather the data...
+    # what file type are we working with?
+    
+    if legend_type != 'single-symbol':
+        if in_file_extension == '.csv' :
+            try:
+                f = open( in_file_fullpath, 'rt' )
+            except IOError as (errno, strerror):
+                print "I/O error({0}): {1}".format(errno, strerror)
+                sys.exit(1)
+            
+            data_raw = []
+            try:
+                reader = csv.DictReader( f )
+                for row in reader:
+                    #print row
+                    #print type( row )
+                    data_raw.append( row )
+            finally:
+                f.close()
+                    
+            for row in data_raw:
+                try:
+                    # if the value of the indicator in this row DOESN'T match the filter value, skip this feature
+                    if filter_field:
+                        if row[ options.filter_field[0][0] ] != options.filter_field[0][1] :
+                            continue
+    
+                    if quantitative and (classification_type != 'unique-value' and classification_type != 'unique-values'):
+                        indicator = float( row[ indicator_field ] )
+                    else:
+                        indicator = row[ indicator_field ]
+                    name = row[ feature_name_field ]
+                    data_clean.append( {'name': name, 'indicator': indicator } )
+                except Exception, e:
+                    print "The following exception occurred : ", e
+            
+        elif in_file_extension == '.shp' :
+            print 'in .shp parse'
+            
+            # Get the shapefile driver
+            driver = ogr.GetDriverByName('ESRI Shapefile')
+            
+            # Open the data source
+            data_raw = driver.Open(in_file_fullpath, 0)
+            
+            if data_raw is None:
+              print 'Could not open ' + in_file_fullpath
+              sys.exit(1)
+            
+            # Get the data layer
+            layer = data_raw.GetLayer()
+            
+            # Get the row
+            feature = layer.GetNextFeature()
+            
+            # counter if there is no name field
+            i = 0
+    
+            # Gather values
+            while feature:
+                # if the value of the indicator in this row DOESN'T match the filter value, skip this feature
+                if filter_field:
+                    if feature.GetField( options.filter_field[0][0] ) != options.filter_field[0][1] :
+                        continue
+            
+                # get the attributes
+                indicator = feature.GetField(indicator_field)
+                try:
+                    name = feature.GetField( feature_name_field )
+                except:
+                    name = i
+                    
+                data_clean.append( {'name': name, 'indicator': indicator } )
+                       
+                # Destroy the feature and get a new one
+                feature.Destroy()
+                feature = layer.GetNextFeature()
+            
+            # Close the data source
+            data_raw.Destroy()
+    else:
+        classification_type = 'single-symbol'
+        data_clean.append( {'name': '', 'indicator': 1 } )
+        data_clean.append( {'name': '', 'indicator': 1 } )
+
+   
+    #print 'data_clean: ', data_clean
+
+    # User just wants to know what's in their data file
+    if legend_type == 'tell-me':
+        print 'Printing stats (one moment)...'
+        
+        # TODO: print out the (field names) and (field types).
+        #OgrInfo(in_file_fullpath)
+        
+        sys.exit(1)
+        
     # If the output directory doesn't exist, make it so we don't error later on file open()
     if not os.path.exists(out_dir):
         print 'making dir...'
         os.makedirs(out_dir)
     
+    # prepare output files
     mss_file = open(out_mss,"w")
     mml_file = open(out_mml,"w")
+    html_file = open(out_html,"w")
     
-    # Set the working directory
-    os.chdir(in_dir)
-    #os.chdir(r'C:\Data\Conferences\UGIC2009\data')
-    
-    # Get the shapefile driver
-    driver = ogr.GetDriverByName('ESRI Shapefile')
-    
-    # Open the data source
-    datasource = driver.Open(in_file, 0)
-    if datasource is None:
-      print 'Could not open ' + in_file
-      sys.exit(1)
-    
-    # Get the data layer
-    layer = datasource.GetLayer()
-    
-    # If no legend type was provided, print info about the file, and make a single-symbol map
-    if not legend_type:
-        print 'Looking for stats on your file? Printing those (one moment)... Then drawing simple single-simple map styles...'
-        
-        # TODO: print out the (field names) and (field types).
-        #ogrinfo(in_file_fullpath)
-        
-        # Proceed and just dump out the data
-        legend_type = 'single-symbol'
+    # Set the working directory (important for the SHP OGR bits?)
+    os.chdir(in_dir)        
     
     # Stub out the MSS (styles)
-    mss_header = ['/* MSS header tk tk tk */']
+    mss_header = ['/* MSS Stylesheet */\n\n']
     mss_footer = ['']
     
     # Stub out the MML (layers)
     mml_header = ['<?xml version="1.0" encoding="utf-8"?>\n<!DOCTYPE Map[\n\t<!ENTITY epsg4326 "+proj=longlat +datum=WGS84">\n\t<!ENTITY epsg900913 "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs +over">\n\t<!ENTITY epsg900913_extent "-20037508,-20037508,20037508,20037508">\n]>\n\n<Map srs="&epsg900913;">\n\n\t<Stylesheet src="stylesheet.mss"/>']
+    mml_layer = ['\n\n\t<Layer class="' + in_file_name_part + '" id="'+ in_file_name_part + '" srs="&epsg4326;">\n\t\t<Datasource>\n\t\t\t<Parameter name="type">shape</Parameter>\n\t\t\t<Parameter name="file">' + in_file + '</Parameter>\n\t\t</Datasource>\n\t</Layer>\n']
     mml_footer = ['\n</Map>']
     
-    
+    # Stub out the HTML (legend)
+    html_header = ['<!DOCTYPE html>\n<html>\n<head>\n<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">\n<title>Map legend</title>\n</head>\n<body >']
+    html_footer = ['</body></html>']
+            
     # Write out the headers to the text files
     mss_file.writelines(mss_header)
     mml_file.writelines(mml_header)
+    html_file.writelines(html_header)    
     
-    mml_layer = ['\n\n\t<Layer class="' + in_file + '" id="'+ in_file + '" srs="&epsg4326;">\n\t\t<Datasource>\n\t\t\t<Parameter name="type">shape</Parameter>\n\t\t\t<Parameter name="file">' + in_file_fullpath + '</Parameter>\n\t\t</Datasource>\n\t</Layer>\n']
-    
-    mml_file.writelines(mml_layer)
-    
+    print 'legend_type: ', legend_type
+    print 'classification_type: ', classification_type
+    print 'indicator: ', indicator_field
     
     # What type of map are we making?
-    if legend_type == 'single-symbol':
-        #points
-        #lines
-        #polygons
-        pass
-    elif legend_type == 'unique-value':
-        # If no fieldname was provided, error
-        if not fieldname:
-            print 'Bad fieldname, exiting.'
+    if classification_type == 'single-symbol':
+        data_classed = Classify( data_clean, False, 'indicator', 5, 'Universal', [], 'indicator', 2, 'None', [], 'YlGn'  )
+    elif classification_type == 'unique-value' or classification_type == 'unique-values':
+        data_classed = Classify( data_clean, False, 'indicator', 5, 'Unique value', [], 'indicator', 2, 'None', [], 'YlGn'  )
+    elif classification_type == 'quantile' or classification_type == 'quantiles':
+        if not quantitative:
+            print 'Can only make Quantiles for numbers fields, exiting...'
             sys.exit(1)
-        
-        stats = gather_stats( layer, fieldname, 10000)
-        
-        # TODO: loop thru the stats.values and make an MSS layer for each entry in Dict
-        
-        #for i in stats.values:
-        print stats
-        
-        #points
-        #lines
-        #polygons
-        
-        #mss_layer = ['#water-bodies[zoom=8][area>50000000] { polygon-fill: #000; }',
-        #'#water-bodies[zoom=9][area>10000000] { polygon-fill: #000; }',
-        #'#water-bodies[zoom=10][area>2500000] { polygon-fill: #000; }']
-
-    elif legend_type == 'continuous-color':
-        # If no fieldname was provided, error
-        if not fieldname:
-            print 'Bad fieldname, exiting.'
+        data_classed = Classify( data_clean, False, 'indicator' , 5, 'Quantiles', [], 'indicator', 2, 'None', [], 'YlGn'  )
+    elif classification_type == 'equal-interval':
+        if not quantitative:
+            print 'Can only make Equal Interval for numbers fields, exiting...'
             sys.exit(1)
-        
-        stats = gather_stats( layer, fieldname, 10000)
-        
-        # TODO: Then quantiles
-                       
-        # Let's look at the first value in the Dict and see if it's a String
-        # TODO
-        # val = 1st element in Dict's name
-        if isinstance(val, str):
-            print 'Can only make quantiles for numbers fields, exiting...'
+        data_classed = Classify( data_clean, False, 'indicator' , 4, 'Equal Interval', [], 'indicator', 2, 'None', [], 'YlGn'  )
+    elif classification_type == 'standard-deviation':
+        if not quantitative:
+            print 'Can only make Standard Deviation for numbers fields, exiting...'
             sys.exit(1)
-            
-        # TODO: Cast the dict to a list ignoring the counts
-     
-        for qtype in range(1,10):
-            print qtype, Quantile(x, 0.35, qtype)
-            
-        # TODO: make the MSS
-                        
-    elif legend_type == 'graduated-symbol':
-         # TODO: et al
+        data_classed = Classify( data_clean, False, 'indicator' , 4, 'Standard Deviation', [], 'indicator', 2, 'None', [], 'YlGn'  )
+    elif classification_type == 'minimum-variance' or classification_type == 'jenks' or classification_type == 'jenks-optimal':
+        if not quantitative:
+            print 'Can only make Minimum Variance for numbers fields, exiting...'
+            sys.exit(1)
+        data_classed = Classify( data_clean, False, 'indicator' , 4, 'Minimum Variance', [], 'indicator', 2, 'None', [], 'YlGn'  )
+    elif classification_type == 'manual':
+        data_classed = Classify( data_clean, False, 'indicator' , 4, 'Manual', [], 'indicator', 2, 'None', [], 'YlGn'  )
+    elif classification_type == 'continuous-color' or classification_type == 'ratio':
+        data_classed = Classify( data_clean, False, 'indicator' , 4, 'Ratio', [], 'indicator', 2, 'None', [], 'YlGn'  )
+    elif classification_type == 'graduated-symbol':
         pass
     else:
-        print 'Bad legend type, exiting.'
+        print 'Bad legend or classification type, exiting.'
         sys.exit(1)
-        
-        
-        
-    # Close the data source
-    datasource.Destroy()
+    
+    #self.classInit( dataTest, False, dataAttrName, 4, 'Equal Interval', testBreaks, dataAttrName, 2, 'None', testNominalBreaks, 'YlGn'  )
+    #self.classInit( dataTest, False, dataAttrName, 5, 'Quantiles', testBreaks, dataAttrName, 2, 'None', testNominalBreaks, 'YlGn'  )
+    #self.classInit( dataTest, False, dataAttrName, 3, 'Quantiles', testBreaks, dataAttrName, 2, 'None', testNominalBreaks, 'YlGn'  )
+    #self.classInit( dataTest, False, dataAttrName, 3, 'Standard Deviation', testBreaks, dataAttrName, 2, 'None', testNominalBreaks, 'YlGn'  )
+    
+    #TODO: fix
+    #self.classInit( dataTest, False, dataAttrName, 3, 'Minimum Variance', testBreaks, dataAttrName, 2, 'Ratio', testNominalBreaks, 'YlGn'  )
+    #self.classInit( dataTest, False, dataAttrName, 3, 'Manual', testBreaks, dataAttrName, 2, 'Ratio', testNominalBreaks, 'YlGn' )
+    
+    #self.classInit( dataTest, False, dataAttrName, 3, 'Ratio', testBreaks, 'pumpType', 2, 'Nominal', testNominalBreaks, 'YlGn' )
+    #self.classInit( dataTest, False, dataAttrName, 3, 'Ratio', testBreaks, 'pumpType', 4, 'None', testNominalBreaks, 'YlGn' )
+    #self.classInit( dataTest, False, dataAttrName, 4, 'Equal Interval', testBreaks, 'pumpType', 2, 'Nominal', testNominalBreaks, 'YlGn'  )
+
+    mss_layers = data_classed.get_mss()
+    if indicator_field:
+        legend_layers = 'legend #' + indicator_field + '<br/>' + data_classed.get_legend()
+    else:
+        legend_layers = 'legend #' + '<br/>' + data_classed.get_legend()
+    
+    # Write out the layer content to the text files
+    mss_file.writelines(mss_layers)
+    mml_file.writelines(mml_layer)    
+    html_file.writelines(legend_layers)
     
     # Write out the footers to the text files
     mss_file.writelines(mss_footer)
     mml_file.writelines(mml_footer)
+    html_file.writelines(html_footer)
     
     # Close the MSS and MML files
     mss_file.close()
     mml_file.close()
-
-
-def gather_stats(layer, fieldname, num_features=10000):
-    statistics = {'values':{}, 'min':{}, 'max':{}, 'type':'', 'count':0, 'count_not_null':0}
-    # Loop through the features in the layer
-    feature = layer.GetNextFeature()
-
-    if type( feature.GetField(fieldname) ) is str:
-        statistics['type'] = 'String'
-    else:
-        statistics['type'] = 'Number'
-    
-    # Gather values
-    while feature:        
-        # get the attributes
-        key = feature.GetField(fieldname)
-        
-        # Add key to dictonary, and track with value counter for total values (histogram)
-        if key in statistics['values']:
-            statistics['values'][ key ] += 1
-        else:
-            statistics['values'][ key ] = 1
-        
-        # Increment the statistics counter
-        statistics['count'] += 1
-        if key:
-            statistics['count_not_null'] += 1
-               
-        # Destroy the feature and get a new one
-        feature.Destroy()
-        feature = layer.GetNextFeature()
-    
-    # Calculate min/max for the data ranges
-    
-    field_values = []
-    field_values_counts = []
-    for k, v in statistics['values'].iteritems():
-        field_values.append(k)
-        field_values_counts.append(v)
-    
-    #print min(field_values)
-    #print max(field_values)
-    #print min(field_values_counts)
-    #print max(field_values_counts)
-    
-    statistics['min'] = min(field_values)
-    statistics['max'] = max(field_values)
-        
-    # TODO: sort the dict and take the last and first
-    
-    # Return the results
-    return statistics
+    html_file.close()
